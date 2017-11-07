110e9f0806dd77c4418f9d1f12dec9401cb23655
郭贵成 <guoguicheng@eschervr.com>
2017-11-07 13:57:06 +0800

    update

--- src/crawler.py
+++ src/crawler.py
@@ -9,26 +9,34 @@ class carwler:
         text=data1.decode(chardit1['encoding']).encode("utf-8")
         return text
 
-    def del_tags(self,str):
+    def delScript(self,page_source):
         pat_script=re.compile("<script([\s\S]*?)>[\s\S]*?<\/script([\s\S]*?)>",re.S)
         pat_style=re.compile("<style([\s\S]*?)>([\s\S]*?)<\/style([\s\S]*?)>",re.S)
-        pat_zhushi=re.compile("<!--([\s\S]*?)-->",re.S)
-        pat_html_tag=re.compile("<.+?>",re.S)
-        pat_null_line=re.compile(r"\s+",re.S)
-        s=pat_script.sub('',str)
+        s=pat_script.sub('',page_source)
         s=pat_style.sub('',s)
-        s=pat_zhushi.sub('',s)
-        s=pat_html_tag.sub(' ',s)
-        s=pat_null_line.sub(' ',s)
         return s
 
-    def getImgBase64Str(self,str):
+    def delComment(self,page_source):
+        pat_comment=re.compile("<!--([\s\S]*?)-->",re.S)
+        s=pat_comment.sub('',page_source)
+        return s
+
+    def getText(self,page_source):
+        s=self.delScript(page_source)
+        s=self.delComment(s)
+        pat_null_line=re.compile(r"\s+",re.S)
+        pat_html_tag=re.compile("<.+?>",re.S)
+        s=pat_null_line.sub(' ',s)
+        s=pat_html_tag.sub(' ',s)
+        return s
+
+    def getImgBase64Str(self,page_source):
         restr=r'<img[\s\S]+?src=[\'\"]data:(.+?)[\'\"][\s\S\>]?'
         pat=re.compile(restr,re.I)
-        ds=re.findall(pat,str)
+        ds=re.findall(pat,page_source)
         return ds
 
-    def getImageList(self,html):
+    def getImageList(self,url,page_source):
         restr=ur'('
         restr+=ur'(http[s]?:\/\/)?[^\s,"]*\.jpg'
         restr+=ur'|(http[s]?:\/\/)?[^\s,"]*\.jpeg'
@@ -42,11 +50,11 @@ class carwler:
         restr+=ur'|(http[s]?:\/\/)?[^\s,"]*\.ico'
         restr+=ur')'
         htmlurl = re.compile(restr,re.I)
-        imgList = re.findall(htmlurl,html)
-        return list(set([Arr[0] for Arr in imgList]))
+        imgList = re.findall(htmlurl,page_source)
+        return list(set([urljoin(url,Arr[0]) for Arr in imgList]))
         #return list(set(imgList))
 
-    def getMediaList(self,html):
+    def getMediaList(self,url,page_source):
         restr=ur'('
         restr+=ur'(http[s]?:\/\/)?[^\s,"]*\.mp3'
         restr+=ur'|(http[s]?:\/\/)?[^\s,"]*\.mp4'
@@ -61,25 +69,19 @@ class carwler:
         restr+=ur'|(http[s]?:\/\/)?[^\s,"]*\.swf'
         restr+=ur')'
         htmlurl = re.compile(restr)
-        mediaList = re.findall(htmlurl,html)
-        return list(set([Arr[0] for Arr in mediaList]))
-        #return list(set(mediaList))
-    def getAllScript(self,html):
+        mediaList = re.findall(htmlurl,page_source)
+        return list(set([urljoin(url,Arr[0]) for Arr in mediaList]))
+
+
+    def getAllScript(self,url,page_source):
         restr=ur'('
         restr+=ur'(http[s]?:\/\/)?[^\s,"]*\.css'
         restr+=ur'|(http[s]?:\/\/)?[^\s,"]*\.js'
         restr+=ur')'
         htmlurl = re.compile(restr)
-        links = re.findall(htmlurl,html)
-        return list(set([Arr[0] for Arr in links]))
-        #return list(set(links))
-    def getAllLinks(self,html):
-        restr=ur'('
-        restr+=ur'http[s]?:\/\/[^\s,"]*'
-        restr+=ur')'
-        htmlurl = re.compile(restr)
-        links = re.findall(htmlurl,html)
-        return list(set(links))
+        links = re.findall(htmlurl,page_source)
+        return list(set([urljoin(url,Arr[0]) for Arr in links]))
+
 class resource:
     def downloadImg(self,url,savePath,imgurl):
         imgurl=urljoin(url,imgurl,savePath)
